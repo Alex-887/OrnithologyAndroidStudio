3a8cd3c8d4073af9d678bdd8ed6ba58c422cd816
Léonard <leo1095@gmail.com>
2020-03-02 11:28:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ornithology_favre_berthouzoz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ornithology_favre_berthouzoz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
