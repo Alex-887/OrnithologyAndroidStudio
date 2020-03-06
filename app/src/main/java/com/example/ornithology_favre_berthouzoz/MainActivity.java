b3743e79a1eb6fef076e5e1b84e6abb50499a5e6
Alex <berthouzozalex@gmail.com>
2020-03-06 14:23:15 +0100

    bonjour

--- app/src/main/java/com/example/ornithology_favre_berthouzoz/MainActivity.java
+++ app/src/main/java/com/example/ornithology_favre_berthouzoz/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.ornithology_favre_berthouzoz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void about(View aboutView) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void searchName(View searchNameView) {
+        Intent intent = new Intent(this, SearchNameActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void searchFamily(View searchFamilyView) {
+        Intent intent = new Intent(this, SearchFamilyActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void searchIdentification(View searchIdentificationView) {
+        Intent intent = new Intent(this, SearchIdentificationActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void settings(View settingsView) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+
+    }
+
 }
