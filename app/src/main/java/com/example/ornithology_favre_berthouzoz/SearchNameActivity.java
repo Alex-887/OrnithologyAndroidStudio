32a2561668b68ea518a525031818d441df8abd34
Léonard <leo1095@gmail.com>
2020-03-08 10:59:00 +0100

    Search name

--- app/src/main/java/com/example/ornithology_favre_berthouzoz/SearchNameActivity.java
+++ app/src/main/java/com/example/ornithology_favre_berthouzoz/SearchNameActivity.java
@@ -22,16 +22,6 @@ public class SearchNameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_name);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
